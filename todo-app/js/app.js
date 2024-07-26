97e53cc92f61e477c4f5f97f85e58ac8c85441cd
Sumit Khandelwal <synergy2411@outlook.com>
2024-07-26 16:29:56 +0530

    todo app added

--- todo-app/js/app.js
+++ todo-app/js/app.js
@@ -1,1 +1,21 @@
-console.log("Loaded...");
+window.onload = function () {
+  let txtInput = document.getElementById("label");
+  let buttonEl = document.querySelector("#btn-add");
+  let listContainer = document.getElementById("list-container");
+
+  buttonEl.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (txtInput.value.trim() === "") {
+      return;
+    }
+    createElement(txtInput.value);
+    txtInput.value = "";
+  });
+
+  function createElement(inputValue) {
+    let liElement = document.createElement("li");
+    liElement.innerText = inputValue;
+    liElement.classList.add("list-item");
+    listContainer.appendChild(liElement);
+  }
+};
